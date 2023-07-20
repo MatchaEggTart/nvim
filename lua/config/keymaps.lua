572249b34bdf76d52b308adc73d4f744d583e0b7
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-07-20 09:55:54 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,5 +11,140 @@ local opt = { noremap = true, silent = true }
 map("n", "<leader>w", ":w<CR>", opt)
 map("n", "<leader>wq", ":wqa!<CR>", opt)
 ---- 退出
--- map("n", "<leader>qq", ":q!<CR>", opt)
+---- map("n", "qq", ":q!<CR>", opt)
 map("n", "<leader>q", ":qa!<CR>", opt)
+
+------------------------------------------------------------------
+-- windows 分屏快捷键
+------------------------------------------------------------------
+-- 取消 s 默认功能
+-- map("n", "s", "", opt)
+-- map("n", "sv", ":vsp<CR>", opt)
+-- map("n", "sh", ":sp<CR>", opt)
+-- 关闭当前
+-- map("n", "sc", "<C-w>c", opt)
+-- 关闭其他
+-- map("n", "so", "<C-w>o", opt) -- close others
+-- alt + hjkl  窗口之间跳转
+map("n", "<C-h>", "<C-w>h", opt)
+map("n", "<C-j>", "<C-w>j", opt)
+map("n", "<C-k>", "<C-w>k", opt)
+map("n", "<C-l>", "<C-w>l", opt)
+-- <leader> + hjkl 窗口之间跳转
+map("n", "<leader>h", "<C-w>h", opt)
+map("n", "<leader>j", "<C-w>j", opt)
+map("n", "<leader>k", "<C-w>k", opt)
+map("n", "<leader>l", "<C-w>l", opt)
+-- 左右比例控制
+-- map("n", "<C-Left>", ":vertical resize -2<CR>", opt)
+-- map("n", "<C-Right>", ":vertical resize +2<CR>", opt)
+-- map("n", "s,", ":vertical resize -10<CR>", opt)
+-- map("n", "s.", ":vertical resize +10<CR>", opt)
+-- 上下比例
+-- map("n", "sj", ":resize +10<CR>", opt)
+-- map("n", "sk", ":resize -10<CR>", opt)
+-- map("n", "<C-Down>", ":resize +2<CR>", opt)
+-- map("n", "<C-Up>", ":resize -2<CR>", opt)
+-- 相等比例
+-- map("n", "s=", "<C-w>=", opt)
+
+-- Terminal相关
+map("n", "st", ":sp | terminal<CR>", opt)
+map("n", "stv", ":vsp | terminal<CR>", opt)
+-- Esc 回 Normal 模式
+map("t", "<Esc>", "<C-\\><C-n>", opt)
+map("t", "<A-h>", [[ <C-\><C-N><C-w>h ]], opt)
+map("t", "<A-j>", [[ <C-\><C-N><C-w>j ]], opt)
+map("t", "<A-k>", [[ <C-\><C-N><C-w>k ]], opt)
+map("t", "<A-l>", [[ <C-\><C-N><C-w>l ]], opt)
+map("t", "<leader>h", [[ <C-\><C-N><C-w>h ]], opt)
+map("t", "<leader>j", [[ <C-\><C-N><C-w>j ]], opt)
+map("t", "<leader>k", [[ <C-\><C-N><C-w>k ]], opt)
+map("t", "<leader>l", [[ <C-\><C-N><C-w>l ]], opt)
+
+-- visual模式下缩进代码
+-- map("v", "<", "<gv", opt)
+-- map("v", ">", ">gv", opt)
+-- 上下移动选中文本
+-- map("v", "J", ":move '>+1<CR>gv-gv", opt)
+-- map("v", "K", ":move '<-2<CR>gv-gv", opt)
+
+-- 在visual mode 里粘贴不要复制
+-- map("v", "p", '"_dP', opt)
+
+-- 上下滚动浏览
+-- map("n", "<C-j>", "5j", opt)
+-- map("n", "<C-k>", "5k", opt)
+-- map("v", "<C-j>", "5j", opt)
+-- map("v", "<C-k>", "5k", opt)
+-- ctrl u / ctrl + d  只移动9行，默认移动半屏
+-- map("n", "<C-u>", "10k", opt)
+-- map("n", "<C-d>", "10j", opt)
+
+-- 退出
+-- map("n", "qq", ":q!<CR>", opt)
+-- map("n", "<leader>q", ":qa!<CR>", opt)
+
+-- insert 模式下，跳到行首行尾
+-- map("i", "<C-h>", "<ESC>I", opt)
+-- map("i", "<C-l>", "<ESC>A", opt)
+
+
+--------------------------------------------------------------------
+-- 插件快捷键
+local pluginKeys = {}
+
+------------------------------------------------------------------
+-- nvim-tree
+------------------------------------------------------------------
+-- map("n", "<C-t>", ":NvimTreeToggle<CR>", opt)
+map("n", "<leader>t", ":NvimTreeToggle<CR>", opt)
+-- 列表快捷键
+pluginKeys.nvimTreeList = { -- 打开文件或文件夹
+  { key = { "o", "<2-LeftMouse>" }, action = "edit" },
+  -- 下面那条设置系统打开
+  -- 既记事本打开文档，文件管理器打开文件夹，剧毒
+  -- { key = "s", action = "system_open" },
+  -- v分屏打开文件
+  -- { key = "v", action = "vsplit" },
+  -- h分屏打开文件
+  -- { key = "h", action = "split" },
+  -- Ignore (node_modules)
+  { key = "i", action = "toggle_ignored" },
+  -- Hide (dotfiles)
+  { key = ".", action = "toggle_dotfiles" },
+  -- 文件操作
+  { key = "r", action = "refresh" },
+  { key = "a", action = "create" },
+  { key = "d", action = "remove" },
+  { key = "m", action = "rename" },
+  { key = "x", action = "cut" },
+  { key = "c", action = "copy" },
+  { key = "p", action = "paste" },
+  { key = "y", action = "copy_name" },
+  { key = "Y", action = "copy_path" },
+  { key = "gy", action = "copy_absolute_path" },
+  { key = "I", action = "toggle_file_info" },
+  { key = "n", action = "tabnew" },
+  -- 进入下一级
+  { key = { "]" }, action = "cd" },
+  -- 进入上一级
+  { key = { "[" }, action = "dir_up" },
+}
+
+------------------------------------------------------------------
+-- bufferline
+------------------------------------------------------------------
+-- 左右Tab切换
+map("n", "<A-h>", ":BufferLineCyclePrev<CR>", opt)
+map("n", "<A-l>", ":BufferLineCycleNext<CR>", opt)
+-- "moll/vim-bbye" 关闭当前 buffer
+map("n", "<leader>bc", ":Bdelete!<CR>", opt)
+-- map("n", "<C-w>", ":Bdelete!<CR>", opt)
+-- 关闭左/右侧标签页
+map("n", "<leader>bh", ":BufferLineCloseLeft<CR>", opt)
+map("n", "<leader>bl", ":BufferLineCloseRight<CR>", opt)
+-- 关闭其他标签页
+map("n", "<leader>bo", ":BufferLineCloseRight<CR>:BufferLineCloseLeft<CR>", opt)
+-- 关闭选中标签页
+map("n", "<leader>bp", ":BufferLinePickClose<CR>", opt)
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,7 +4,47 @@ return {
     version = "*",
     dependencies = {"nvim-tree/nvim-web-devicons"},
     config = function()
-      require("nvim-tree").setup {}
+      require("nvim-tree").setup {
+        sort_by = "case_sensitive",
+        view = {
+          adaptive_size = true,
+          mappings = {
+            list = {
+              -- { key = "u", action = "dir_up" },
+            },
+          },
+          -- 宽度
+          width = 40,
+          -- 也可以 'right'
+          side = 'left',
+          -- 隐藏根目录
+          hide_root_folder = false,
+          -- 不显示行数
+          number = false,
+          relativenumber = false,
+          -- 显示图标
+          signcolumn = 'yes',
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = true,
+          -- 隐藏 .文件 和 node_modules 文件夹
+          custom = { 'node_modules' },
+        },
+        git = {
+          enable = true,
+        },
+        actions = {
+          open_file = {
+          -- 首次打开大小适配
+          resize_window = true,
+          -- 打开文件时关闭
+          -- quit_on_open = true,
+          },
+        }
+      }
     end,
   }
 }
