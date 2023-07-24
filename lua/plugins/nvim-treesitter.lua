4f4aa7e7a1dc86beab7cba7ce4aefc8cf5d0e20e
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-07-24 16:39:45 +0800

    update

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+        ensure_installed = {
+          "vim",
+          "vimdoc",
+          "lua",
+          "luadoc",
+          "markdown",
+          "markdown_inline",
+          "bash",
+          "c",
+          "java",
+          "rust",
+          "python", 
+          "html",
+          "css",
+          "javascript",
+          "typescript",
+          "tsx",
+          "query",
+          "regex",
+          "yaml", -- sudo dnf install g++ -y
+        },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },  
+      })
+    end
+  }
+}
