cb7b9a1f9aa4dab63937ac899dbc533c557e9c37
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-08-07 15:15:47 +0800

    update

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,8 +3,20 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+    },
     config = function()
+      require("tokyonight").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        light_style = "day", -- The theme is used when the background is set to light
+        transparent = true, -- Enable this to disable setting the background color
+        styles = {
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "transparent", -- style for sidebars, see below
+        }
+      })
       -- load the colorscheme here
       vim.cmd([[colorscheme tokyonight]])
     end,
