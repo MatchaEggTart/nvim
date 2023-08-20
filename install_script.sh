d01b69c74943df6e3ef7844653a6061ae53746ca
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-08-20 19:53:47 +0800

    update

---
+++ install_script.sh
@@ -0,0 +1,4 @@
+# install the emmet
+sudo npm install -g emmet-ls                                                                                                                                                             ─╯
+# install typescript
+# sudo npm install -g vscode-html-languageservice
