d599a763011d968f4f6e6bde87e4df8ef15b49b2
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-08-22 14:31:02 +0800

    update

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -13,6 +13,48 @@ return {
     keys = {
       { "<leader>t", "<cmd>Neotree toggle<CR>", desc = "Neotree" },
     },
+    config = function()
+      -- code
+      require("neo-tree").setup({
+        default_component_configs = {
+          icon = {
+            folder_closed = "",
+            folder_open = "",
+            -- folder_empty = "󰜌",
+            folder_empty = "empty folder ",
+            -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+            -- then these will never be used.
+            default = "*",
+            highlight = "NeoTreeFileIcon"
+          },
+          modified = {
+            symbol = "[+]",
+            highlight = "NeoTreeModified",
+          },
+          name = {
+            trailing_slash = false,
+            use_git_status_colors = true,
+            highlight = "NeoTreeFileName",
+          },
+          git_status = {
+            symbols = {
+              -- Change type
+              added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+              modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+              deleted   = "✖", -- this can only be used in the git_status source
+              renamed   = "󰁕", -- this can only be used in the git_status source
+              -- Status type
+              untracked = "",
+              ignored   = "",
+              -- unstaged  = "󰄱",
+              unstaged  = "U",
+              staged    = "",
+              conflict  = "",
+            }
+          },
+        },
+      })
+    end
   },
 
   --[[
